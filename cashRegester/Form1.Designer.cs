c72287a96d99db50d77d67cc687ad2185f3b26ba
Dylan Smith <dylasmit288@ed.amdsb.ca>
2021-09-30 14:07:41 -0400

    Add project files.

---
+++ cashRegester/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace cashRegester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.titleLabel2 = new System.Windows.Forms.Label();
+            this.inputLabel1 = new System.Windows.Forms.Label();
+            this.inputLabel2 = new System.Windows.Forms.Label();
+            this.inputLabel3 = new System.Windows.Forms.Label();
+            this.hatsInput = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.subTotalLabel = new System.Windows.Forms.Label();
+            this.taxLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.subTotalLabel2 = new System.Windows.Forms.Label();
+            this.taxLabel2 = new System.Windows.Forms.Label();
+            this.totalLabel2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Constantia", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(21, 22);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(198, 45);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "DEMENTE";
+            // 
+            // titleLabel2
+            // 
+            this.titleLabel2.AutoSize = true;
+            this.titleLabel2.Font = new System.Drawing.Font("Constantia", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel2.Location = new System.Drawing.Point(74, 67);
+            this.titleLabel2.Name = "titleLabel2";
+            this.titleLabel2.Size = new System.Drawing.Size(169, 19);
+            this.titleLabel2.TabIndex = 1;
+            this.titleLabel2.Text = "STREET WEAR SHOP";
+            // 
+            // inputLabel1
+            // 
+            this.inputLabel1.AutoSize = true;
+            this.inputLabel1.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputLabel1.Location = new System.Drawing.Point(25, 137);
+            this.inputLabel1.Name = "inputLabel1";
+            this.inputLabel1.Size = new System.Drawing.Size(168, 19);
+            this.inputLabel1.TabIndex = 2;
+            this.inputLabel1.Text = "Number of Bucket Hats";
+            // 
+            // inputLabel2
+            // 
+            this.inputLabel2.AutoSize = true;
+            this.inputLabel2.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputLabel2.Location = new System.Drawing.Point(25, 183);
+            this.inputLabel2.Name = "inputLabel2";
+            this.inputLabel2.Size = new System.Drawing.Size(168, 19);
+            this.inputLabel2.TabIndex = 3;
+            this.inputLabel2.Text = "Number of Cargo Pants";
+            // 
+            // inputLabel3
+            // 
+            this.inputLabel3.AutoSize = true;
+            this.inputLabel3.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputLabel3.Location = new System.Drawing.Point(25, 227);
+            this.inputLabel3.Name = "inputLabel3";
+            this.inputLabel3.Size = new System.Drawing.Size(172, 19);
+            this.inputLabel3.TabIndex = 4;
+            this.inputLabel3.Text = "Number of Graphic Tees";
+            // 
+            // hatsInput
+            // 
+            this.hatsInput.Location = new System.Drawing.Point(199, 136);
+            this.hatsInput.Name = "hatsInput";
+            this.hatsInput.Size = new System.Drawing.Size(70, 20);
+            this.hatsInput.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(199, 182);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(70, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(199, 226);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(70, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // calcButton
+            // 
+            this.calcButton.Font = new System.Drawing.Font("Microsoft JhengHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calcButton.Location = new System.Drawing.Point(78, 268);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(141, 33);
+            this.calcButton.TabIndex = 8;
+            this.calcButton.Text = "Calculate Totals";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // subTotalLabel
+            // 
+            this.subTotalLabel.AutoSize = true;
+            this.subTotalLabel.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subTotalLabel.Location = new System.Drawing.Point(25, 318);
+            this.subTotalLabel.Name = "subTotalLabel";
+            this.subTotalLabel.Size = new System.Drawing.Size(77, 19);
+            this.subTotalLabel.TabIndex = 9;
+            this.subTotalLabel.Text = "Sub Total ";
+            // 
+            // taxLabel
+            // 
+            this.taxLabel.AutoSize = true;
+            this.taxLabel.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taxLabel.Location = new System.Drawing.Point(25, 351);
+            this.taxLabel.Name = "taxLabel";
+            this.taxLabel.Size = new System.Drawing.Size(34, 19);
+            this.taxLabel.TabIndex = 10;
+            this.taxLabel.Text = "Tax";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalLabel.Location = new System.Drawing.Point(25, 384);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(44, 19);
+            this.totalLabel.TabIndex = 11;
+            this.totalLabel.Text = "Total";
+            this.totalLabel.Click += new System.EventHandler(this.totalLabel_Click);
+            // 
+            // subTotalLabel2
+            // 
+            this.subTotalLabel2.AutoSize = true;
+            this.subTotalLabel2.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subTotalLabel2.Location = new System.Drawing.Point(195, 318);
+            this.subTotalLabel2.Name = "subTotalLabel2";
+            this.subTotalLabel2.Size = new System.Drawing.Size(13, 19);
+            this.subTotalLabel2.TabIndex = 12;
+            this.subTotalLabel2.Text = " ";
+            // 
+            // taxLabel2
+            // 
+            this.taxLabel2.AutoSize = true;
+            this.taxLabel2.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taxLabel2.Location = new System.Drawing.Point(195, 351);
+            this.taxLabel2.Name = "taxLabel2";
+            this.taxLabel2.Size = new System.Drawing.Size(13, 19);
+            this.taxLabel2.TabIndex = 13;
+            this.taxLabel2.Text = " ";
+            // 
+            // totalLabel2
+            // 
+            this.totalLabel2.AutoSize = true;
+            this.totalLabel2.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalLabel2.Location = new System.Drawing.Point(195, 384);
+            this.totalLabel2.Name = "totalLabel2";
+            this.totalLabel2.Size = new System.Drawing.Size(13, 19);
+            this.totalLabel2.TabIndex = 14;
+            this.totalLabel2.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.totalLabel2);
+            this.Controls.Add(this.taxLabel2);
+            this.Controls.Add(this.subTotalLabel2);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.taxLabel);
+            this.Controls.Add(this.subTotalLabel);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.hatsInput);
+            this.Controls.Add(this.inputLabel3);
+            this.Controls.Add(this.inputLabel2);
+            this.Controls.Add(this.inputLabel1);
+            this.Controls.Add(this.titleLabel2);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label titleLabel2;
+        private System.Windows.Forms.Label inputLabel1;
+        private System.Windows.Forms.Label inputLabel2;
+        private System.Windows.Forms.Label inputLabel3;
+        private System.Windows.Forms.TextBox hatsInput;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.Label subTotalLabel;
+        private System.Windows.Forms.Label taxLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label subTotalLabel2;
+        private System.Windows.Forms.Label taxLabel2;
+        private System.Windows.Forms.Label totalLabel2;
+    }
+}
+
---
+++ cashRegester/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cashRegester
+{
+    public partial class Form1 : Form
+    {
+        double buckethat = 12.00;
+        double cargopants = 15.25;
+        double graphictees = 10.50;
+
+        int numOfhats = 0;
+        int numOfpants = 0;
+        int numOftees = 0;
+
+        double subtotal = 0;
+        double totalprice = 0;
+
+        double taxrate = 0.13;
+        double taxamount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                numOfhats = Convert.ToInt32(hatsInput.Text);
+                numOfpants = Convert.ToInt32(hatsInput.Text);
+                numOftees = Convert.ToInt32(hatsInput.Text);
+
+                subtotal = numOfhats * buckethat + numOfpants * cargopants + numOftees * graphictees;
+                taxamount = taxrate;
+                totalprice = subtotal * taxamount;
+
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void totalLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
